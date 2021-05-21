4d5dd202482efa1bbccb9168680a437fbe3104d6
Cody <codyp424@gmail.com>
2021-05-21 14:43:23 -0600

    v1.1.0

--- connector/main.py
+++ connector/main.py
@@ -5,6 +5,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--backfill", help="Backfill entire database",action="store_true")
 parser.add_argument("--backfillAll", help="Backfill entire database",action="store_true")
+parser.add_argument("--daily", help="Run daily calculations",action="store_true")
+
 
 args = parser.parse_args()
 
@@ -13,6 +15,10 @@ observations = Observations(stations.stationId)
 
 if args.backfillAll:
 	observations.backFillAll(stations.deviceId)
+	observations.dailyBackfill(stations.deviceId,Full = True)
+
+elif args.daily:
+	observations.dailyBackfill(stations.deviceId,Full = False)
 
 elif args.backfill:
 	observations.backFill(stations.deviceId)
--- connector/structs/observations.py
+++ connector/structs/observations.py
@@ -1,7 +1,9 @@
 import rest.util as util
 import psycopg2
 import time
+import datetime
 from time import gmtime,strftime
+from dateutil import tz
 
 class Observations:
 	
@@ -44,46 +46,78 @@ class Observations:
 		return (cel * 1.8) + 32
 
 
-	def daily(self, deviceId):
+	def dailyBackfill(self, deviceId, Full = False):
 
+		print "Rebuilding entire database"
+		i = 0
 		conn = psycopg2.connect("dbname='weather' user='postgres' host='127.0.0.1' password='postgres'")
 		cur = conn.cursor()
 
-		cur.execute("""SELECT * FROM data.statistics ORDER BY timestamp DESC LIMIT 1""")
-		record = cur.fetchall()
-		lastTime = record[0][-2]
+		cur.execute("""SELECT * FROM data.statistics ORDER BY timestamp DESC""")
+		records = cur.fetchall()
+		
+		from_zone = tz.gettz('UTC')
+		to_zone = tz.gettz('America/Chicago')
 
-		if time.time()-60 > lastTime:
-			daysSince = int(((time.time() - lastTime) / 60 / 60 / 24)) + 1
+		today = datetime.date.today()
 
-			for i in range(daysSince):
-				self.data = util.get('observations/device/%d/?day_offset=%d' % (deviceId,i))
-				for item in self.data['obs']:
-					if int(item[0]) > lastTime:
-						try:
-							cur.execute("""INSERT INTO data."statistics"("timestamp","air_temperature","barometric_pressure","station_pressure","sea_level_pressure","relative_humidity","precip","precip_minutes_local_day","wind_avg","wind_direction","wind_gust","solar_radiation","uv","brightness","lightning_strike_count","feels_like","heat_index","wind_chill","dew_point","air_density") VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""", \
-							(float(item[0]),	float(self.toF(item[7])),
-							float(item[6]),
-							float(item[6]),	
-							float(0),	
-							int(item[8]),	
-							float(item[18]),	
-							int(item[17]),	
-							float(item[2]),	
-							float(item[4]),	
-							float(item[3]),	
-							int(item[11]),	
-							float(item[10]),	
-							int(item[9]),	
-							int(item[15]),	
-							float(self.toF(item[7])),	
-							float(self.toF(item[7])),	
-							float(self.toF(item[7])),	
-							float(0),	
-							float(0)))
-							conn.commit()
-						except:
-							pass
+
+		high = -999.0
+		low = 999.0 
+		rain = 0.0
+		day = -1
+
+		last = None
+		if not Full:
+			cur.execute("""SELECT * FROM data.calculations ORDER BY timestamp DESC LIMIT 1""")
+			record = cur.fetchall()
+			lastTime = record[0][-1]
+
+			last = datetime.datetime.fromtimestamp(lastTime)
+			u = last.replace(tzinfo=from_zone)
+			last = u.astimezone(to_zone)
+
+
+		for record in records:
+			date = datetime.datetime.fromtimestamp(record[-2])
+			utc = date.replace(tzinfo=from_zone)
+			date = utc.astimezone(to_zone)
+
+			if date != today:
+				if date.day != day:
+					day = date.day
+					high = -999
+					low = 999 
+					rain = 0.0
+
+				if record[0] > high:
+					high = record[0]
+
+				if record[0] < low:
+					low = record[0]
+
+				if record[5] > rain:
+					rain = record[5]
+
+				if not Full:
+					if date <= last:
+						print date
+						print last
+						break 
+
+				if date.hour == 0 and date.minute == 0:
+					try:
+						cur.execute("""INSERT INTO data."calculations"("timestamp","high","low","rain") VALUES (%s,%s,%s,%s);""", \
+						(record[-2], high,
+						low,
+						rain
+						))
+						conn.commit()
+					except Exception as e:
+						pass
+
+				
+
 
 	def backFill(self, deviceId):
 
